2bf412fd727f659b54fad856478891337a1c5710
jainnpriyanshii <priyanshijain664@gmail.com>
2025-11-03 08:58:39 +0530

    feat: Add Activity, Aesthetic, Color Preference, Create, Profile, and Room Type screens with basic layout

---
+++ Components/Screens/AestheticScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AestheticScreen = () => {
+  return (
+    <View>
+      <Text>AestheticScreen</Text>
+    </View>
+  )
+}
+
+export default AestheticScreen
